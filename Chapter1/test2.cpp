57c4b21b557a0463effcf2234f5aba6428f7a13e
Thinh P. Tran <trphthinh@gmail.com>
2017-07-08 18:01:52 +0700

    Don't have to delete vector.

---
+++ Chapter1/test2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector> 
+
+using namespace std;
+
+int main() {
+  size_t vsize = 10; 
+  vector<int> vec(vsize);
+
+  for (int iter = 0; iter < vsize; iter++) {
+    vec[iter] = iter; 
+  }
+
+  for (int iter = 0; iter < vsize; iter++) {
+    cout << "vec: " << vec[iter] << endl; 
+  }
+
+  //!!! No need to delete anything.
+}
+
